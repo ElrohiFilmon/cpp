cf1a3f0a04e0448b433206ed3bae906dfcf957fa
ElrohiFilmon <elrohifilmon@gmail.com>
2024-04-24 19:36:26 +0300

    small Class project

---
+++ database/database.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+struct student{
+    string name;
+    int age;
+    char gender;
+    string batch;
+};
+void read(int &n){
+    cout<<"How many records do you want to enter"<<endl;
+    cin>>n;
+    student *s = new student[n];
+    fstream ifile;  
+    ifile.open("student_data.dat", ios::binary);
+    cout<<"----Enter your records----"<<endl<<"Notice: Fill them respectivly!"<<endl<<"1:Name"<<endl<<"2:Age"<<endl<<"3:gender(M/F)"<<endl<<"4:Batch"<<endl;
+    for(int i =0,j=1;i<n;++i,++j){
+        cout<<"Record"<<j<<endl;
+        cin>>s[i].name;
+        cin>>s[i].age;
+        cin>>s[i].gender;
+        cin>>s[i].batch;
+        ifile.write((char*)&s[i], sizeof(student));
+    } 
+    ifile.close();
+}
+
+void display(const int n){
+    cout<<" 1:Name"<<"  2:Age"<< " 3:gender(M/F)"<<" 4:Batch"<<endl; 
+    student *a; 
+    fstream ofile;
+    ofile.open("student_data.dat", ios::binary);
+   
+    for(int i =0,j=1;i<n;++i,++j){        
+        ofile.read((char*)&a[i],sizeof (student));
+        cout<<" "<<a[i].name<<"    "<<a[i].age<<"    "<<a[i].gender<<"             "<<a[i].batch<<endl;
+    }
+    ofile.close();
+}
+
+int main(){
+    int n;
+    student a; 
+    read(n);
+    display(n);
+}
